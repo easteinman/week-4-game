fe1dd7587fc5ad677853311a0d4b6be6896266aa
Eric <ericsteinman@me.com>
2017-12-17 20:48:24 -0600

    Updated JS file to have an onClick event to increase user total. Going to work on this more tomorrow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,30 @@ $( document ).ready(function(){
 
   $(".displayNumber").text(randomNumber);
 
+  // Scoreboard Variables
+  var score = 0;
+
+  // Sending the score variable to the score counter
+  $('.scoreCounter').text(score);
+
   // Assign a random number between 1-12 to each crystal
   var crystalNum1= Math.floor(Math.random()*11+1)
   var crystalNum2= Math.floor(Math.random()*11+1)
   var crystalNum3= Math.floor(Math.random()*11+1)
   var crystalNum4= Math.floor(Math.random()*11+1)
 
+  // Set up a click event for each crystal that increases the user total by the number of the crystal.
+  $("#crystal1").on("click", function(){ 
+    score = score + crystalNum1;
+    $(".scoreCounter").text(score);
+      if (score == randomNumber) {
+      	alert("Congratulations, you collected to correct amount of crystals!");
+      }
+
+      else if (score > randomNumber) {
+      	alert("Whoops, you took too many crystals. You lose.");
+      }
+  })
 
   // Win/Lose Variables 
   var wins = 0;
@@ -20,12 +38,5 @@ $( document ).ready(function(){
   // Sending the wins and losses variables to the page
   $('.winCounter').text("Wins: " + wins);
   $('.lossCounter').text("Losses: " + losses);
-
-
-  // Scoreboard Variables
-  var score = 0;
-
-  // Sending the score variable to the score counter
-  $('.scoreCounter').text(score);
 
 });
