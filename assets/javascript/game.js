8ebd6517b96a4317992650170b30c76972889a06
Eric <ericsteinman@me.com>
2017-12-18 16:29:24 -0600

    Added a reset function and fixed the Score div so it now keeps track of the score correctly. Game is finished.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,128 @@
 $( document ).ready(function(){
 	console.log( "document loaded" );
 
-  //Random Number Variable
+  // Random Number Variable (between 19-120)
   var randomNumber = Math.floor(Math.random()*101+19)
-
+  
+  // Send this number to the Random Number div
   $(".displayNumber").text(randomNumber);
 
-  // Scoreboard Variables
-  var score = 0;
+  // User total Variable
+  var userTotal = 0;
 
-  // Sending the score variable to the score counter
-  $('.scoreCounter').text(score);
+  // Send the userTotal variable to the userTotal div
+  $(".totalCounter").text(userTotal);
 
   // Assign a random number between 1-12 to each crystal
-  var crystalNum1= Math.floor(Math.random()*11+1)
-  var crystalNum2= Math.floor(Math.random()*11+1)
-  var crystalNum3= Math.floor(Math.random()*11+1)
-  var crystalNum4= Math.floor(Math.random()*11+1)
+  var crystalNum1 = Math.floor(Math.random()*11+1)
+  var crystalNum2 = Math.floor(Math.random()*11+1)
+  var crystalNum3 = Math.floor(Math.random()*11+1)
+  var crystalNum4 = Math.floor(Math.random()*11+1)
+
+  // Scoreboard Variables 
+  var wins = 0;
+  var losses = 0;
 
   // Set up a click event for each crystal that increases the user total by the number of the crystal.
   
   // Crystal 1
   $("#crystal1").on("click", function(){ 
-    score = score + crystalNum1;
-    $(".scoreCounter").text(score);
-      if (score == randomNumber) {
+    userTotal = userTotal + crystalNum1;
+    $(".totalCounter").text(userTotal);
+
+      if (userTotal == randomNumber) {
+      	wins++;
+      	$(".winCounter").text("Wins: " + wins);
       	alert("Congratulations, you collected to correct amount of crystals! You WIN!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
 
-      else if (score > randomNumber) {
+      else if (userTotal > randomNumber) {
+      	losses++;
+      	$(".lossCounter").text("Losses: " + losses);
       	alert("Whoops, you took too many crystals. You LOSE!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
   })
 
   // Crystal 2
   $("#crystal2").on("click", function(){ 
-    score = score + crystalNum2;
-    $(".scoreCounter").text(score);
-      if (score == randomNumber) {
+    userTotal = userTotal + crystalNum2;
+    $(".totalCounter").text(userTotal);
+
+      if (userTotal == randomNumber) {
+      	wins++;
+      	$(".winCounter").text("Wins: " + wins);
       	alert("Congratulations, you collected to correct amount of crystals! You WIN!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
 
-      else if (score > randomNumber) {
+      else if (userTotal > randomNumber) {
+      	losses++;
+      	$(".lossCounter").text("Losses: " + losses);
       	alert("Whoops, you took too many crystals. You LOSE!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
   })
 
   // Crystal 3
   $("#crystal3").on("click", function(){ 
-    score = score + crystalNum3;
-    $(".scoreCounter").text(score);
-      if (score == randomNumber) {
+    userTotal = userTotal + crystalNum3;
+    $(".totalCounter").text(userTotal);
+
+      if (userTotal == randomNumber) {
+      	wins++;
+      	$(".winCounter").text("Wins: " + wins);
       	alert("Congratulations, you collected to correct amount of crystals! You WIN!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
 
-      else if (score > randomNumber) {
+      else if (userTotal > randomNumber) {
+      	losses++;
+      	$(".lossCounter").text("Losses: " + losses);
       	alert("Whoops, you took too many crystals. You LOSE!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
   })
 
   // Crystal 4
   $("#crystal4").on("click", function(){ 
-    score = score + crystalNum4;
-    $(".scoreCounter").text(score);
-      if (score == randomNumber) {
+    userTotal = userTotal + crystalNum4;
+    $(".totalCounter").text(userTotal);
+
+      if (userTotal == randomNumber) {
+      	wins++;
+      	$(".winCounter").text("Wins: " + wins);
       	alert("Congratulations, you collected to correct amount of crystals! You WIN!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
 
-      else if (score > randomNumber) {
+      else if (userTotal > randomNumber) {
+      	losses++;
+      	$(".lossCounter").text("Losses: " + losses);
       	alert("Whoops, you took too many crystals. You LOSE!");
+      	// Reset randomNumber, crystalNums, and userTotal
+      	reset();
       }
   })
 
-  // Win/Lose Variables 
-  var wins = 0;
-  var losses = 0;
-
-  // Sending the wins and losses variables to the page
-  $('.winCounter').text("Wins: " + wins);
-  $('.lossCounter').text("Losses: " + losses);
+  // Created a reset function that only resets the variables that need to be reset at the end of each game so the user doesn't have to refresh the page to play again.
+  function reset(){
+    randomNumber = Math.floor(Math.random()*101+19)
+    $(".displayNumber").text(randomNumber);
+    userTotal = 0;
+    $(".totalCounter").text(userTotal);
+    crystalNum1 = Math.floor(Math.random()*11+1)
+    crystalNum2 = Math.floor(Math.random()*11+1)
+    crystalNum3 = Math.floor(Math.random()*11+1)
+    crystalNum4  = Math.floor(Math.random()*11+1)
+  }
 
 });
